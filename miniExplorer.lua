c8681103f676334407dbc84a989d5bc0e43e1bb6
Vivian Shen <vivianhshen@gmail.com>
2016-02-13 19:31:47 -0500

    test
    starting script for my

---
+++ miniExplorer.lua
@@ -0,0 +1,24 @@
+scriptId = 'com.example.miniExplorer'
+scriptTitle = "Mini Explorer"
+scriptDetailsUrl = --add url here later, apparently from the market website
+platform = "MacOS"
+
+function onPoseEdge(pose, edge)
+    myo.debug("onPoseEdge: " .. pose .. ", " .. edge)
+end
+
+function onPeriodic()
+end
+
+function onForegroundWindowChange(app, title)
+    myo.debug("onForegroundWindowChange: " .. app .. ", " .. title)
+    return true
+end
+
+function activeAppName()
+    return "Output Everything"
+end
+
+function onActiveChange(isActive)
+    myo.debug("onActiveChange")
+end
